ddfcd829593e8d9871f9690525c9171df5a97b12
Yakov Zaytsev <me@yakovzaytsev.com>
2018-08-11 14:14:06 +0300

    get YSZ_SEED or set rand.Seed

--- pkg/ysz/ysz.go
+++ pkg/ysz/ysz.go
@@ -2,7 +2,23 @@ package ysz
 
 import (
 	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
+
+func init() {
+	seed_str, ok := os.LookupEnv("YSZ_SEED")
+	if ok {
+		seed, err := strconv.Atoi(seed_str)
+		if err != nil {
+			panic(err)
+		}
+		rand.Seed(int64(seed))
+	} else {
+		rand.Seed(time.Now().UnixNano())
+	}
+}
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
